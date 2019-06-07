bc819625a1102ecac81b985ddaf1f66b6baf9472
GladeousLazy <naviteshvaswani@gmail.com>
2019-06-07 23:57:48 +0530

    added new bot with complete intent with entity

--- ACE_Bot.py
+++ ACE_Bot.py
@@ -5,45 +5,22 @@ import json
 from virtassnt.nodes import *
 from virtassnt.core import Node, Message
 from virtassnt.graph import Graph
-import pandas as pd #Navitesh - This line was added to create custom dataframe for the Dalia bot
 
-#Navitesh - Below is a sample data from for testing out the bot, the data contains year, month and sales value
-df = pd.DataFrame([[2018,'jan',1000],[2019,'jan',2000],[2017,'jan',3000],[2018,'feb',4000]],columns = ['year','month','sales'])
 
-class CustomAction_show_value(Node):
+class CustomAction_generate_trend(Node):
 
 	def __init__(self, name):
 		self.pprint = PrettyPrinter(indent=4)
-		super(CustomAction_show_value, self).__init__(name)
+		super(CustomAction_generate_trend, self).__init__(name)
 
-#Navitesh - The below piece of code is updated to accomodate the custom response
+
 	def call(self, incoming_msg):
 		data=incoming_msg.data
 		#Put API Logic Here
-		entities = data["entities"]
-		year = entities["year"]
-		month = entities["month"]
 
-		def convert_res(row):
-			return {"year" : row["year"], "month" : row["month"] , sales : row[sales]}
-
-		res = products.apply(convert_res, axis=1)
-
-		##Navitesh -  This is the original peice of code ##  results = {'for_response': results}
-
-        #Navitesh - Below is the new piece of code that creates the desired response.
-        results = {
-			"for_response" : {
-				"results" : {
-				"type" : "text",
-                "data" : "The year selected is " + year + " and the month selected is " +  month,
-                "baseName" : "year"
-                }
-			}
-		}
-				
+		results = {'for_response': results}
 		outgoing_msg=Message.merge([incoming_msg, Message(results)])
-		return super(CustomAction_show_value, self).call(outgoing_msg)
+		return super(CustomAction_generate_trend, self).call(outgoing_msg)
 def build_bot(user_id='navitesh.vaswani@saama.com'):
 
 	graph=Graph(name='navitesh.vaswani@saama.com_ACE_Bot', user_id=user_id)
@@ -53,59 +30,81 @@ def build_bot(user_id='navitesh.vaswani@saama.com'):
 	greet_user = Input('How are you', text_only=True, name='greet_user')
 	graph.add_node(greet_user)
 
-	ytd_sales = NLUParser(name='ytd_sales', model_path='./ytd_sales', config_path='./resources/config_spacy.json')
-	graph.add_node(ytd_sales)
+	ask_for_option = NLUParser(name='ask_for_option', model_path='./ask_for_option', config_path='./resources/config_spacy.json')
+	graph.add_node(ask_for_option)
 
-	check_for_yr_mth_master_validate = Validator(required_keys=['year', 'month'], name='check_for_yr_mth_master_validate')
-	graph.add_node(check_for_yr_mth_master_validate)
+	trend_chart_master_validate = Validator(required_keys=['duration', 'year', 'month', 'measure'], name='trend_chart_master_validate')
+	graph.add_node(trend_chart_master_validate)
 
-	year_validate = Validator(required_keys=['year'], name='year_validate')
-	graph.add_node(year_validate)
+	trend_chart_duration_validate = Validator(required_keys=['duration'], name='trend_chart_duration_validate')
+	graph.add_node(trend_chart_duration_validate)
 
-	year_input = Input('Enter year', text_only=True, name='year_input')
-	graph.add_node(year_input)
+	trend_chart_duration_input = Input('Enter duration', text_only=True, name='trend_chart_duration_input')
+	graph.add_node(trend_chart_duration_input)
 
-	year_parse = NLUParser(name='year_parse', model_path='./year_parse', config_path='./resources/config_spacy.json')
-	graph.add_node(year_parse)
+	trend_chart_duration_parse = NLUParser(name='trend_chart_duration_parse', model_path='./trend_chart_duration_parse', config_path='./resources/config_spacy.json')
+	graph.add_node(trend_chart_duration_parse)
 
-	month_validate = Validator(required_keys=['month'], name='month_validate')
-	graph.add_node(month_validate)
+	trend_chart_year_validate = Validator(required_keys=['year'], name='trend_chart_year_validate')
+	graph.add_node(trend_chart_year_validate)
 
-	month_input = Input('Enter month', text_only=True, name='month_input')
-	graph.add_node(month_input)
+	trend_chart_year_input = Input('Enter year', text_only=True, name='trend_chart_year_input')
+	graph.add_node(trend_chart_year_input)
 
-	month_parse = NLUParser(name='month_parse', model_path='./month_parse', config_path='./resources/config_spacy.json')
-	graph.add_node(month_parse)
+	trend_chart_year_parse = NLUParser(name='trend_chart_year_parse', model_path='./trend_chart_year_parse', config_path='./resources/config_spacy.json')
+	graph.add_node(trend_chart_year_parse)
 
-	show_value=CustomAction_show_value('show_value')
-	graph.add_node(show_value)
-	end = End()
-	graph.add_node(end)
+	trend_chart_month_validate = Validator(required_keys=['month'], name='trend_chart_month_validate')
+	graph.add_node(trend_chart_month_validate)
 
-	final_response = Response(name='final_response')
-	final_response.add_response_pattern('[result]')
-	graph.add_node(final_response)
+	trend_chart_month_input = Input('Enter month', text_only=True, name='trend_chart_month_input')
+	graph.add_node(trend_chart_month_input)
 
-	exitnode = Input('Do you have any other request?', text_only=True, name='exitnode')
-	graph.add_node(exitnode)
+	trend_chart_month_parse = NLUParser(name='trend_chart_month_parse', model_path='./trend_chart_month_parse', config_path='./resources/config_spacy.json')
+	graph.add_node(trend_chart_month_parse)
+
+	trend_chart_measure_validate = Validator(required_keys=['measure'], name='trend_chart_measure_validate')
+	graph.add_node(trend_chart_measure_validate)
+
+	trend_chart_measure_input = Input('Enter measure', text_only=True, name='trend_chart_measure_input')
+	graph.add_node(trend_chart_measure_input)
+
+	trend_chart_measure_parse = NLUParser(name='trend_chart_measure_parse', model_path='./trend_chart_measure_parse', config_path='./resources/config_spacy.json')
+	graph.add_node(trend_chart_measure_parse)
+
+	generate_trend=CustomAction_generate_trend('generate_trend')
+	graph.add_node(generate_trend)
+	show_trend = Response(name='show_trend')
+	show_trend.add_response_pattern('[results]')
+	graph.add_node(show_trend)
+
+	exit = End()
+	graph.add_node(exit)
 
 	#Making the Connections
 
-	greet_user.add_output(ytd_sales)
-	ytd_sales.add_output('check_for_yr_mth', check_for_yr_mth_master_validate)
-	check_for_yr_mth_master_validate.add_output(show_value, True)
-	check_for_yr_mth_master_validate.add_output(year_validate, False)
-	year_validate.add_output(month_validate, True)
-	year_validate.add_output(year_input, False)
-	year_input.add_output(year_parse)
-	year_parse.add_output('year', year_validate)
-	month_validate.add_output(month_input, True)
-	month_validate.add_output(show_value, False)
-	month_input.add_output(month_parse)
-	month_parse.add_output('month', month_validate)
-	show_value.add_output(final_response)
-	final_response.add_output(exitnode)
-	exitnode.add_output(end)
+	greet_user.add_output(ask_for_option)
+	ask_for_option.add_output('trend_chart', trend_chart_master_validate)
+	trend_chart_master_validate.add_output(generate_trend, True)
+	trend_chart_master_validate.add_output(trend_chart_duration_validate, False)
+	trend_chart_duration_validate.add_output(trend_chart_year_validate, True)
+	trend_chart_duration_validate.add_output(trend_chart_duration_input, False)
+	trend_chart_duration_input.add_output(trend_chart_duration_parse)
+	trend_chart_duration_parse.add_output('duration', trend_chart_duration_validate)
+	trend_chart_year_validate.add_output(trend_chart_month_validate, True)
+	trend_chart_year_validate.add_output(trend_chart_year_input, False)
+	trend_chart_year_input.add_output(trend_chart_year_parse)
+	trend_chart_year_parse.add_output('year', trend_chart_year_validate)
+	trend_chart_month_validate.add_output(trend_chart_measure_validate, True)
+	trend_chart_month_validate.add_output(trend_chart_month_input, False)
+	trend_chart_month_input.add_output(trend_chart_month_parse)
+	trend_chart_month_parse.add_output('month', trend_chart_month_validate)
+	trend_chart_measure_validate.add_output(generate_trend, True)
+	trend_chart_measure_validate.add_output(trend_chart_measure_input, False)
+	trend_chart_measure_input.add_output(trend_chart_measure_parse)
+	trend_chart_measure_parse.add_output('measure', trend_chart_measure_validate)
+	generate_trend.add_output(show_trend)
+	show_trend.add_output(exit)
 
 	#Complete the Build
 	graph.build_completed()
